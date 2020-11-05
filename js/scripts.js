172ff2fba7f9f37e3338c8a422ce32269159daf8
punith-gowda <poppunith2001@gmail.com>
2020-11-05 19:15:54 +0530

    smooth scrolling added

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,3 @@
-
-
 (function ($) {
     "use strict";
 
@@ -16,7 +14,7 @@
         hidePreloader();
     });
 
-        // closes the responsive menu on menu item click
+    // closes the responsive menu on menu item click
     $(".navbar-nav li a").on("click", function (event) {
         if (!$(this).parent().hasClass('dropdown'))
             $(".navbar-collapse").collapse('hide');
@@ -30,7 +28,24 @@
             $('a.back-to-top').fadeIn('500');
         } else {
             $('a.back-to-top').fadeOut('500');
-        }   
+        }
     });
 
 })(jQuery);
+
+$(function () {
+    // This will select everything with the class smoothScroll
+    // This should prevent problems with carousel, scrollspy, etc...
+    $('.smoothScroll').click(function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: target.offset().top
+                }, 1000); // The number here represents the speed of the scroll in milliseconds
+                return false;
+            }
+        }
+    });
+});
